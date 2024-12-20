9b57d95455f5397d2f099aaf28c3a38320102a00
Joshua Pan <joshua.pan@affirm.com>
2024-12-20 14:28:03 -0500

    solve day19

---
+++ day19_part2.py
@@ -0,0 +1,27 @@
+import sys
+sys.setrecursionlimit(1073741824)
+
+def works(d, ts, seen):
+    if len(d) == 0: return 1
+    if d in seen:
+        return seen[d]
+    w = 0
+    for t in ts:
+        if len(d) >= len(t) and d.startswith(t):
+            w += works(d[len(t):], ts, seen)
+    seen[d] = w
+    return w
+
+
+with open('day19_input.txt') as f:
+    line = f.read().strip()
+    lines = line.split('\n\n')
+    ts = lines[0].strip().split(', ')
+    ds = [d.strip() for d in lines[1].split('\n')]
+
+    seen = {}
+    ans = 0
+    for d in ds:
+        ans += works(d, ts, seen)
+    print(ans)
+
